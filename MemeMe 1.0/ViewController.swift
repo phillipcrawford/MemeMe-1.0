1858798d7c09284f4381c68e7e1ca6034aa2f74d
[01;31m[K"name"[m[K: "Phillip Crawford <[01;31m[K"email"[m[K: "phillipcrawford3@gmail.com>
2016-04-22 05:51:34 -0700

    add action for pick button: launches imagePickerController

--- MemeMe 1.0/ViewController.swift
+++ MemeMe 1.0/ViewController.swift
@@ -15,11 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func pickAnImageFromAlbum(sender: AnyObject) {
+        let pickerController = UIImagePickerController()
+        self.presentViewController(pickerController, animated: true, completion: nil)
     }
-
-
 }
 
