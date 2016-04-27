1723137ef00d5c3ed8ce3ec61e13c486dc708a9d
[01;31m[K"name"[m[K: "Phillip Crawford <[01;31m[K"email"[m[K: "phillipcrawford3@gmail.com>
2016-04-27 06:11:50 -0700

    refactor keyboardWillShow and keyboardWillHide

--- MemeMe 1.0/MemeEditorViewController.swift
+++ MemeMe 1.0/MemeEditorViewController.swift
@@ -110,13 +110,13 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
     
     func keyboardWillShow(notification: NSNotification) {
         if bottomTextField.isFirstResponder() {
-            view.frame.origin.y -= getKeyboardHeight(notification)
+            view.frame.origin.y = getKeyboardHeight(notification) * -1
         }
     }
     
     func keyboardWillHide(notification: NSNotification) {
         if bottomTextField.isFirstResponder() {
-            view.frame.origin.y += getKeyboardHeight(notification)
+            view.frame.origin.y = 0
         }
     }
     
